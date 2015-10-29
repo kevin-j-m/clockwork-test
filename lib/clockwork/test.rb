ad7c2ff5e650acd6110ab757e2bc1f7d745540f7
Kevin Murphy <kevin.murphy@meyouhealth.com>
2015-10-28 21:21:52 -0400

    Gem creation

---
+++ lib/clockwork/test.rb
@@ -0,0 +1,7 @@
+require "clockwork/test/version"
+
+module Clockwork
+  module Test
+    # Your code goes here...
+  end
+end
---
+++ lib/clockwork/test/version.rb
@@ -0,0 +1,5 @@
+module Clockwork
+  module Test
+    VERSION = "0.0.1"
+  end
+end
